1fffb200d3e655f16b0f88fb20eeee6da76dd07b
Thanushka Jayasinghe <Thanushkajayasinghe@gmail.com>
2021-06-08 10:35:29 +0530

    Update MPIController.java

--- src/main/java/com/mpi/hl7/controller/MPIController.java
+++ src/main/java/com/mpi/hl7/controller/MPIController.java
@@ -19,6 +19,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,6 +54,11 @@ public class MPIController {
 	@Autowired
 	private LogHl7Repo logHl7Repo;
 
+	@GetMapping("/")
+	public String welcome(){
+		return "Welcome to MPI Cloud Solution!";	
+	}
+	
 	@PostMapping("/save")
 	public ResponseEntity<?> saveHl7(@RequestBody hl7send hl7data) throws HL7Exception, JSONException {
 
